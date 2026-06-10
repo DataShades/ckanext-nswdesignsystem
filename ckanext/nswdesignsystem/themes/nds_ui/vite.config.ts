2d3f8601fcc66c3e0c8bb38a59c5b97c8ea34d44
Sergey Motornyuk <sergey.motornyuk@linkdigital.com.au>
2026-06-10 10:21:35 +0300

    chore: fix bundling

--- ckanext/nswdesignsystem/themes/nds_ui/vite.config.ts
+++ ckanext/nswdesignsystem/themes/nds_ui/vite.config.ts
@@ -2,15 +2,28 @@ import { defineConfig } from "vite";
 import { resolve } from "path";
 
 export default defineConfig({
-    publicDir: false,
-    build: {
-        lib: {
-            entry: resolve(__dirname, "assets/src/main.ts"),
-            formats: ["iife"],
-            fileName: (format) => `ui.js`,
-            name: "ThemingUI",
-        },
-        outDir: "assets",
-        emptyOutDir: false,
+  publicDir: false,
+  plugins: [
+    {
+      name: "prepend-semicolon",
+      generateBundle(options, bundle) {
+        for (const chunk of Object.values(bundle)) {
+          if (chunk.type === "chunk") {
+            chunk.code = ";" + chunk.code + "\n";
+          }
+        }
+      },
     },
+  ],
+
+  build: {
+    lib: {
+      entry: resolve(__dirname, "assets/src/main.ts"),
+      formats: ["iife"],
+      fileName: (format) => `ui.js`,
+      name: "ThemingUI",
+    },
+    outDir: "assets",
+    emptyOutDir: false,
+  },
 });
